31b7496c0f863f2ba838d3ff3f1d9dec741b8e6a
Filip <55151209+masingbackstage@users.noreply.github.com>
2023-11-14 22:29:23 +0100

    deleted tabs, added buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,27 +34,23 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             this.dGV2 = new System.Windows.Forms.DataGridView();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.dGV3 = new System.Windows.Forms.DataGridView();
-            this.Gauss = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dGV1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dGV2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dGV3)).BeginInit();
-            this.Gauss.SuspendLayout();
-            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(1313, 7);
+            this.button1.Location = new System.Drawing.Point(1336, 50);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "wektor";
+            this.button1.Text = "Gauss";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -63,7 +59,7 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             this.dGV1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dGV1.GridColor = System.Drawing.SystemColors.ControlText;
-            this.dGV1.Location = new System.Drawing.Point(6, 7);
+            this.dGV1.Location = new System.Drawing.Point(12, 12);
             this.dGV1.Name = "dGV1";
             this.dGV1.RowHeadersWidth = 51;
             this.dGV1.Size = new System.Drawing.Size(1301, 351);
@@ -74,7 +70,7 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             this.dGV2.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dGV2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dGV2.GridColor = System.Drawing.Color.SlateGray;
-            this.dGV2.Location = new System.Drawing.Point(6, 465);
+            this.dGV2.Location = new System.Drawing.Point(12, 470);
             this.dGV2.Name = "dGV2";
             this.dGV2.RowHeadersWidth = 51;
             this.dGV2.Size = new System.Drawing.Size(1301, 95);
@@ -82,7 +78,7 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(1313, 36);
+            this.numericUpDown1.Location = new System.Drawing.Point(1336, 15);
             this.numericUpDown1.Minimum = new decimal(new int[] {
             1,
             0,
@@ -101,80 +97,60 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             // dGV3
             // 
             this.dGV3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dGV3.Location = new System.Drawing.Point(6, 364);
+            this.dGV3.Location = new System.Drawing.Point(12, 369);
             this.dGV3.Name = "dGV3";
             this.dGV3.Size = new System.Drawing.Size(1301, 95);
             this.dGV3.TabIndex = 5;
             // 
-            // Gauss
+            // button2
             // 
-            this.Gauss.Controls.Add(this.tabPage1);
-            this.Gauss.Controls.Add(this.tabPage2);
-            this.Gauss.Controls.Add(this.tabPage3);
-            this.Gauss.Controls.Add(this.tabPage4);
-            this.Gauss.Location = new System.Drawing.Point(12, 12);
-            this.Gauss.Name = "Gauss";
-            this.Gauss.SelectedIndex = 0;
-            this.Gauss.Size = new System.Drawing.Size(1399, 601);
-            this.Gauss.TabIndex = 6;
+            this.button2.Location = new System.Drawing.Point(1336, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Hilbert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // tabPage1
+            // button3
             // 
-            this.tabPage1.Controls.Add(this.dGV1);
-            this.tabPage1.Controls.Add(this.numericUpDown1);
-            this.tabPage1.Controls.Add(this.dGV3);
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Controls.Add(this.dGV2);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1391, 575);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Gauss";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(1336, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Cykliczna";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // tabPage2
+            // button4
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1391, 575);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Hilbert";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1391, 575);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Cylkiczna";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1391, 575);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Vandermond";
-            this.tabPage4.UseVisualStyleBackColor = true;
+            this.button4.Location = new System.Drawing.Point(1336, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Vandermond";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1423, 616);
-            this.Controls.Add(this.Gauss);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dGV2);
+            this.Controls.Add(this.dGV3);
+            this.Controls.Add(this.dGV1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dGV1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dGV2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dGV3)).EndInit();
-            this.Gauss.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -186,11 +162,9 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
         private System.Windows.Forms.DataGridView dGV2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.DataGridView dGV3;
-        private System.Windows.Forms.TabControl Gauss;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,13 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
         {
             InitializeComponent();
         }
-
+        /*
+        Size - rozmiar macierzy, wektora
+        x - zmienna ukryta
+        cykl - zmienna urojona do przesuwania liczb w macierzy
+         */
         public int size = 1 ,x;
-        public Complex suma, cykl;
+        public Complex cykl;
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -44,37 +48,8 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
                 }
             }
             x = 0;
-            
-            Complex[,] Hilbert = new Complex[size, size];
-            for(int k = 0; k < size; k++)
-            {
-                for(int w = 0; w < size; w++)
-                {
-                    Hilbert[k, w] = 1 / (k + w - 1);
-                }
-            }
 
-            Complex[,] cykliczna = new Complex[size, size];
-            for (int k = 0; k < size; k++)
-            {
-                for (int w = size; w >= size; w--)
-                {
-                    cykl = cykliczna[k, w];
-                    cykliczna[k, w] = cykliczna[k, w-1];
-                    cykliczna[k, w - 1] = cykl;
-                }
-            }
-
-            Complex[,] Vandermonde = new Complex[size, size];
-            for (int k = 0; k < size; k++)
-            {
-                for (int w = 0; w < size; w++)
-                {
-                    Vandermonde[k, w] = Math.Pow((k + 1),w);
-                }
-            }
-
-            //dGV1 print
+            //dGV1 wyswietlenie macierzy Gaussa
             dGV1.RowCount = size;
             dGV1.ColumnCount = size;
             dGV1.RowHeadersWidth = 80;
@@ -88,7 +63,7 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
                 }
             }
 
-            //dGV2 print
+            //dGV2 wyswietlenie wektora ukrytych x
             dGV2.RowCount = 1;
             dGV2.ColumnCount = size;
             dGV2.RowHeadersWidth = 80;
@@ -98,8 +73,8 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
                 dGV2.Columns[k].HeaderCell.Value = " x = " + (k+1).ToString();
                 dGV2.Rows[0].Cells[k].Value = wynik[k].ToString();
             }
-            
-            //dGV3 print
+
+            //dGV3 wyswietlenie macierzy A*x=B
             dGV3.RowCount = 1;
             dGV3.ColumnCount = size;
             dGV3.RowHeadersWidth = 80;
@@ -111,14 +86,54 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             }
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Macierz Hilberta
+            Complex[,] Hilbert = new Complex[size, size];
+            for (int k = 0; k < size; k++)
+            {
+                for (int w = 0; w < size; w++)
+                {
+                    Hilbert[k, w] = 1 / (k + w - 1);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Macierz cykliczna
+            Complex[,] cykliczna = new Complex[size, size];
+            for (int k = 0; k < size; k++)
+            {
+                for (int w = size; w >= size; w--)
+                {
+                    cykl = cykliczna[k, w];
+                    cykliczna[k, w] = cykliczna[k, w - 1];
+                    cykliczna[k, w - 1] = cykl;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Macierz Vandermonda
+            Complex[,] Vandermonde = new Complex[size, size];
+            for (int k = 0; k < size; k++)
+            {
+                for (int w = 0; w < size; w++)
+                {
+                    Vandermonde[k, w] = Math.Pow((k + 1), w);
+                }
+            }
+
+        }
+
+        //Zmiana rozmiaru macierzy
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
             size = (int)numericUpDown1.Value;
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
-            
-        }
+
     }
 }
