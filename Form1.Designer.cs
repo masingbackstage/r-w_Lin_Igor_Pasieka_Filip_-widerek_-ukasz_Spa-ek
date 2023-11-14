ce786d6301b326729f2591b23096436432bd4326
Filip <55151209+masingbackstage@users.noreply.github.com>
2023-11-14 21:48:32 +0100

    orzel1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,18 +33,19 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             this.dGV1 = new System.Windows.Forms.DataGridView();
             this.dGV2 = new System.Windows.Forms.DataGridView();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dGV3 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dGV1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dGV2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(951, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(1336, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "wektor";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,35 +56,32 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             this.dGV1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dGV1.GridColor = System.Drawing.SystemColors.ControlText;
-            this.dGV1.Location = new System.Drawing.Point(16, 15);
-            this.dGV1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dGV1.Location = new System.Drawing.Point(12, 12);
             this.dGV1.Name = "dGV1";
             this.dGV1.RowHeadersWidth = 51;
-            this.dGV1.Size = new System.Drawing.Size(927, 246);
+            this.dGV1.Size = new System.Drawing.Size(1318, 200);
             this.dGV1.TabIndex = 1;
             // 
             // dGV2
             // 
             this.dGV2.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dGV2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dGV2.Location = new System.Drawing.Point(16, 289);
-            this.dGV2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dGV2.Location = new System.Drawing.Point(12, 401);
             this.dGV2.Name = "dGV2";
             this.dGV2.RowHeadersWidth = 51;
-            this.dGV2.Size = new System.Drawing.Size(927, 250);
+            this.dGV2.Size = new System.Drawing.Size(1318, 203);
             this.dGV2.TabIndex = 3;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(951, 124);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown1.Location = new System.Drawing.Point(1336, 41);
             this.numericUpDown1.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(100, 22);
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
             this.numericUpDown1.TabIndex = 4;
             this.numericUpDown1.Value = new decimal(new int[] {
             1,
@@ -92,21 +90,30 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             0});
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
+            // dGV3
+            // 
+            this.dGV3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV3.Location = new System.Drawing.Point(12, 229);
+            this.dGV3.Name = "dGV3";
+            this.dGV3.Size = new System.Drawing.Size(1318, 150);
+            this.dGV3.TabIndex = 5;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1423, 616);
+            this.Controls.Add(this.dGV3);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.dGV2);
             this.Controls.Add(this.dGV1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dGV1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dGV2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -117,6 +124,7 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
         private System.Windows.Forms.DataGridView dGV1;
         private System.Windows.Forms.DataGridView dGV2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dGV3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -30,7 +30,19 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
                    Wektor[w, k] = new Complex(w + 1, k + 1);
                 }
             }
+            Complex[] wynik = new Complex[size];
+            for (int k = 0; k < size; k++)
+            {
+                x++;
+                for (int w = 0; w < size; w++)
+                {
+                    wynik[k] += Wektor[k,w] * x;
+                }
+                
+            }
+            x = 0;
 
+            //dGV1 print
             dGV1.RowCount = size;
             dGV1.ColumnCount = size;
             dGV1.RowHeadersWidth = 80;
@@ -44,31 +56,29 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
                 }
             }
 
-            Complex[] F0 = new Complex[size];
-            Complex[] wynik = new Complex[size];
-            for (int k = 0; k < size; k++)
-            {
-                x++;
-                for (int w = 0; w < size; w++)
-                {
-                    F0[k] = Wektor[k,w] * x;
-                    suma += F0[k];
-                }
-                wynik[k] = suma;
-                suma = 0;
-            }
-            x = 0;
-
+            //dGV2 print
             dGV2.RowCount = 1;
             dGV2.ColumnCount = size;
             dGV2.RowHeadersWidth = 80;
             for (int k = 0; k < size; k++)
             {
                 dGV2.Rows[0].HeaderCell.Value = " w = 0";
-                dGV2.Columns[k].HeaderCell.Value = " k = " + k.ToString();
+                dGV2.Columns[k].HeaderCell.Value = " x = " + (k+1).ToString();
                 dGV2.Rows[0].Cells[k].Value = wynik[k].ToString();
             }
+            
+            //dGV3 print
+            dGV3.RowCount = 1;
+            dGV3.ColumnCount = size;
+            dGV3.RowHeadersWidth = 80;
+            for (int k = 0; k < size; k++)
+            {
+                dGV3.Rows[0].HeaderCell.Value = " w = 0";
+                dGV3.Columns[k].HeaderCell.Value = " x = " + (k + 1).ToString();
+                dGV3.Rows[0].Cells[k].Value = (k + 1).ToString();
+            }
         }
+
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
             size = (int)numericUpDown1.Value;
