6d69d81ca2cac494a01d4bc5e399f4eb89f20610
Filip <55151209+masingbackstage@users.noreply.github.com>
2023-11-14 22:43:21 +0100

    gauss works

--- Form1.cs
+++ Form1.cs
@@ -25,6 +25,47 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
         public int size = 1 ,x;
         public Complex cykl;
 
+        void Print_dGV(Complex[,] A, Complex[] B)
+        {
+            //dGV1 wyswietlenie macierzy
+            dGV1.RowCount = size;
+            dGV1.ColumnCount = size;
+            dGV1.RowHeadersWidth = 80;
+            for (int k = 0; k < size; k++)
+            {
+                dGV1.Columns[k].HeaderCell.Value = " k = " + k.ToString();
+                for (int w = 0; w < size; w++)
+                {
+                    dGV1.Rows[w].HeaderCell.Value = " w = " + w.ToString();
+                    dGV1.Rows[w].Cells[k].Value = A[w, k].ToString();
+                }
+            }
+
+            //dGV2 wyswietlenie wektora ukrytych x
+            dGV2.RowCount = 1;
+            dGV2.ColumnCount = size;
+            dGV2.RowHeadersWidth = 80;
+            for (int k = 0; k < size; k++)
+            {
+                dGV2.Rows[0].HeaderCell.Value = " w = 0";
+                dGV2.Columns[k].HeaderCell.Value = " x = " + (k + 1).ToString();
+                dGV2.Rows[0].Cells[k].Value = B[k].ToString();
+            }
+
+            //dGV3 wyswietlenie macierzy 
+            dGV3.RowCount = 1;
+            dGV3.ColumnCount = size;
+            dGV3.RowHeadersWidth = 80;
+            for (int k = 0; k < size; k++)
+            {
+                dGV3.Rows[0].HeaderCell.Value = " w = 0";
+                dGV3.Columns[k].HeaderCell.Value = " x = " + (k + 1).ToString();
+                dGV3.Rows[0].Cells[k].Value = (k + 1).ToString();
+            }
+        }
+
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             //Macierz Gaussa
@@ -49,41 +90,7 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
             }
             x = 0;
 
-            //dGV1 wyswietlenie macierzy Gaussa
-            dGV1.RowCount = size;
-            dGV1.ColumnCount = size;
-            dGV1.RowHeadersWidth = 80;
-            for (int k = 0; k < size; k++)
-            {
-                dGV1.Columns[k].HeaderCell.Value = " k = " + k.ToString();
-                for (int w = 0; w < size; w++)
-                {
-                    dGV1.Rows[w].HeaderCell.Value = " w = " + w.ToString();
-                    dGV1.Rows[w].Cells[k].Value = Gauss[w, k].ToString();
-                }
-            }
-
-            //dGV2 wyswietlenie wektora ukrytych x
-            dGV2.RowCount = 1;
-            dGV2.ColumnCount = size;
-            dGV2.RowHeadersWidth = 80;
-            for (int k = 0; k < size; k++)
-            {
-                dGV2.Rows[0].HeaderCell.Value = " w = 0";
-                dGV2.Columns[k].HeaderCell.Value = " x = " + (k+1).ToString();
-                dGV2.Rows[0].Cells[k].Value = wynik[k].ToString();
-            }
-
-            //dGV3 wyswietlenie macierzy A*x=B
-            dGV3.RowCount = 1;
-            dGV3.ColumnCount = size;
-            dGV3.RowHeadersWidth = 80;
-            for (int k = 0; k < size; k++)
-            {
-                dGV3.Rows[0].HeaderCell.Value = " w = 0";
-                dGV3.Columns[k].HeaderCell.Value = " x = " + (k + 1).ToString();
-                dGV3.Rows[0].Cells[k].Value = (k + 1).ToString();
-            }
+            Print_dGV(Gauss, wynik);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -97,6 +104,8 @@ namespace rów_Lin_Igor_Pasieka_Filip_Świderek_Łukasz_Spałek
                     Hilbert[k, w] = 1 / (k + w - 1);
                 }
             }
+
+           
         }
 
         private void button3_Click(object sender, EventArgs e)
